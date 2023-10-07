c87d82f5fb2f9749a52ec996122568fa35932650
Ajarek <ajarek@poczta.onet.pl>
2023-10-07 23:31:42 +0200

    added Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import Image from 'next/image'
+import Photo from 'public/my-logo.png'
+import Link from 'next/link'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar bg-primary text-primary-content h-20 flex justify-between px-4 '>
+         <Link href='/' >
+         <Image
+            src={Photo}
+            width={50}  
+            height={50}
+            alt='logo'
+            className=''
+          />
+          <h2 className='text-bold text-2xl ml-2'>@Jarek</h2>
+         </Link>
+         
+          <div>
+          <button className='text-bold text-2xl ml-2' >Sign In</button>
+          </div>
+    </nav>
   )
 }
 
