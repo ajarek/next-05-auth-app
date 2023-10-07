1d80063d64b52486bc051951875afb91c52ae3ac
Ajarek <ajarek@poczta.onet.pl>
2023-10-08 01:05:11 +0200

    added signIn singOut

--- app/layout.js
+++ app/layout.js
@@ -1,23 +1,26 @@
 import Navbar from '@/components/Navbar'
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import AuthProvider from '@/components/AuthProvider/AuthProvider'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Auth using NextAuth.js',
-  description: ' Next.js 13 Authentication using NextAuth.js, GitHub Sign-In & Tailwind CSS',
+  description:
+    ' Next.js 13 Authentication using NextAuth.js, GitHub Sign-In & Tailwind CSS',
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <div className='max-w-5xl  mx-auto h-screen bg-white'>
-        <Navbar/>
-        {children}
-        </div>
-        </body>
+        <AuthProvider>
+          <div className='max-w-5xl  mx-auto h-screen bg-white'>
+            <Navbar />
+            {children}
+          </div>
+        </AuthProvider>
+      </body>
     </html>
   )
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,53 @@
+'use client'
+
 import React from 'react'
 import Image from 'next/image'
 import Photo from 'public/my-logo.png'
 import Link from 'next/link'
+import { signIn, signOut, useSession } from 'next-auth/react'
 const Navbar = () => {
+  const session = useSession()
   return (
     <nav className='navbar bg-primary text-primary-content h-20 flex justify-between px-4 '>
-         <Link href='/' >
-         <Image
-            src={Photo}
-            width={50}  
-            height={50}
-            alt='logo'
-            className=''
-          />
-          <h2 className='text-bold text-2xl ml-2'>@Jarek</h2>
-         </Link>
-         
-          <div>
-          <button className='text-bold text-2xl ml-2' >Sign In</button>
-          </div>
+      <Link href='/'>
+        <Image
+          src={Photo}
+          width={50}
+          height={50}
+          alt='logo'
+          className=''
+        />
+        <h2 className='text-bold text-2xl ml-2'>@Jarek</h2>
+      </Link>
+
+      <div>
+        {session.status === 'authenticated' ? (
+          <>
+          <button
+            className={'text-red-200 text-bold text-2xl mr-2'}
+            onClick={()=>signOut()}
+          >
+            Sign Out
+          </button>
+          <Image
+          src={session.data.user.image}
+          width={40}
+          height={40}
+          alt='logo'
+          className='border-2 border-rose-200 rounded-full p-1 '
+        />
+          </>
+        ) : (
+          <button
+            onClick={() => {
+              signIn('github')
+            }}
+            className='text-green-200 text-bold text-2xl ml-2'
+          >
+            Sign In
+          </button>
+        )}
+      </div>
     </nav>
   )
 }
